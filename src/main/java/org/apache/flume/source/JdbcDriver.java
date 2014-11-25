a54aec9a7bc46013571fbcf290bbbc5febe5bcd7
lazaromedina <lazaro.medina@gmail.com>
2014-11-25 09:26:00 +0100

    added support for jdbc drivers,postgresl,sqlite,etc

---
+++ src/main/java/org/apache/flume/source/JdbcDriver.java
@@ -0,0 +1,15 @@
+
+package org.apache.flume.source;
+
+/**
+ *
+ * @author luis lazaro
+ */
+public enum JdbcDriver {
+    MYSQL,
+    POSTGRESQL,
+    SQLITE,
+    ORACLE,
+    DERBY,
+    SQLSERVER
+}
